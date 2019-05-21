4aa5c6b85f5ac66f4fcf974889979d5966865498
Guru Prasath <guru98@live.com>
2019-05-21 17:43:00 +0530

    refactoring and ready for serial comm

--- ui/settings.py
+++ ui/settings.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtOpenGL, uic
+from serial import Serial, SerialException 
+from serial.tools import list_ports
 
 qtCreatorFile = "ui/settings.ui"
 Ui_SettingsDialog, QtBaseClass = uic.loadUiType(qtCreatorFile)
@@ -9,9 +11,20 @@ class settingsDialog(QtWidgets.QDialog, Ui_SettingsDialog):
         super(settingsDialog,self).__init__(parent)
         self.setWindowFlags(QtCore.Qt.Dialog)
         self.setupUi(self)
+        ports = [(port.device,port) for port in list_ports.comports()]
+        ports.sort(key=lambda x: int(x[0].split("COM")[1]))
+        [opt.addItem(port[0],port[1]) for port in ports for opt in self.findChildren(QtWidgets.QComboBox)]
 
     def accept(self):
         print("OK")
+        for row in range(0,self.settingsList.rowCount()):
+            l = self.settingsList.itemAt(row,QtWidgets.QFormLayout.LabelRole).widget()
+            d = self.settingsList.itemAt(row,QtWidgets.QFormLayout.FieldRole).widget()
+            print(l.text(),d.currentData()) # d.currentData()
+
+        # set serial ports to sensors
+        # check sensors comm
+        
         self.done(1)
 
     def reject(self):
